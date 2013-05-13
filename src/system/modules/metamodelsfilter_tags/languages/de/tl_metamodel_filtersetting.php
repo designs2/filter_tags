83239042bc1cd5a658b612feaf268751864d9247
Mini Model <minimodel@metamodel.me>
2013-05-13 23:11:38 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsfilter_tags/languages/de/tl_metamodel_filtersetting.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:14:36+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0'] = 'Nur verbleibende Tags';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1'] = 'Zeige nur Optionen, die zugeordnet sind, nachdem der Filter ausgeführt wurde.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['0']     = 'Nur zugeordnete Tags';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['1']     = 'Zeige nur Optionen, die im MetaModel bereits zugeordnet sind.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['tags'] = 'Mehrfachauswahl';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['0']        = 'ODER';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['1']        = 'ODER-Verknüpfung der Tags. Wird automatisch gesetzt, wenn das Attribut vom Typ "Auswahl" ist.';
