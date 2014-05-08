fec29c1851226c406d74f659702293e9f03cbbe1
Mini Model <minimodel@metamodel.me>
2014-05-08 05:16:19 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsfilter_tags/languages/fr/tl_metamodel_filtersetting.php
+++ src/system/modules/metamodelsfilter_tags/languages/fr/tl_metamodel_filtersetting.php
@@ -8,9 +8,14 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T07:59:04+02:00
+ * last-updated: 2013-07-10T10:07:29+02:00
  */
 
-
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0'] = 'Étiquettes restantes seulement.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1'] = 'Montrer seulement les options qui sont encore assignées quelque part après la définition du filtre.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['0']     = 'Etiquettes attribuées seulement ';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlyused']['1']     = 'Montrer seulement les options qui sont assignées quelque part dans le MetaModel.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['tags'] = 'Sélection multiple';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['0']        = 'OR';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['useor']['1']        = 'OR-linking de l\'étiquette. Automatiquement définit si l\'attribut est du type sélection.';
+
